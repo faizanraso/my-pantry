f2d76237547b860c3c4465a54fbae7670b27e6e7
Faizan Rasool <faizanrasool@Faizans-MBP.cogeco.local>
2022-08-15 11:35:07 -0400

    Added functions to retreive, save and remove data from asyncstorage

--- app/assets/colours.tsx
+++ app/assets/colours.tsx
@@ -1,10 +1,10 @@
 export default {
-    primary: '#0e1117',
-    secondary: '#171A21',
-    headerFont: '#C9CFD7',
-    textFont: '#C9CFD7',
-    activeTab: '#A397E7',
-    inactiveTab: '#CAD0D8',
-    navigationBorder: '#41434A',
+    primary: "#0e1117",
+    secondary: "#171A21",
+    headerFont: "#C9CFD7",
+    textFont: "#C9CFD7",
+    activeTab: "#A397E7",
+    inactiveTab: "#CAD0D8",
+    navigationBorder: "#41434A",
 
 }
--- app/screens/MyPantryScreen.tsx
+++ app/screens/MyPantryScreen.tsx
@@ -1,15 +1,51 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import colours from "../assets/colours";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
 export default function MyPantryScreen() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  const storeData = async (key: string, value: any) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const getData = async (key: string) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      console.log("no data");
+      // error reading value
+    }
+  };
+
+  const removeValue = async (key: string) => {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (e) {
+      // remove error
+    }
+    console.log("Done.");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}></ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colours.primary,
+  },
+  scrollView: {
+    marginHorizontal: "5%",
   },
 });
